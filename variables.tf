b1718f3a8a74245c962f36c37002cca252cc0144
root <root@edge-server.us-central1-a.c.varsityhype.internal>
2023-05-10 10:17:37 +0000

    LABTEST

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "namespace" {
+  description = "TERRAFORM LABTEST FOR INTERVIEW"
+  default     = "LABTEST"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+  type        = string
+}
